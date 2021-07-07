0b0ef7434aa8cab51f2865b92b017d82ee04125e
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-07-07 12:23:33 -0400

    commit tree a8df961b9c1eda0b2c096425328eef1ce39f174e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,32 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: [], 
+}, action) {
+    switch(action.type) {
+        // add friend action 
+
+        case "ADD_FRIEND":
+         return (
+            {...state,
+            friends: [
+                ...state.friends, 
+                action.friend
+            ] }
+        )
+
+        // delete friend
+        case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+
+
+      default:
+        return state;
+    }
 }
